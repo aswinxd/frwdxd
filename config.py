86218dcfcc78588c239c244decc73ab7c6a10020
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-03-23 19:43:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN","6765987978:AAHuqASdsQPFUV2O28nbx9nbM0QbT
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI","mongodb+srv://mdalizadeh16:lavos@cluster0.u21tcwa.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP = int(os.environ.get("LOG_GROUP","-1002055628777"))
 ADMIN_USERS_ID = list(map(int, os.environ.get("ADMIN_USERS_ID","6854172577").split()))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL","@Mallu_Playlist"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL","-1002023444138"))
